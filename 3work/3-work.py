4ca11f7771f7490344eaeaf90f9b8c5d7773603c
asus <guhirggo@gmail.com>
2025-04-26 11:07:41 +0300

    requirements.txt

---
+++ 3work/3-work.py
@@ -0,0 +1,24 @@
+text = "VF VF VF sd sds sd"
+
+gop = text.split("1")
+print(gop)
+
+pass
+# mani = 2250
+#
+#
+# DISCOUNT_0_1000 = 0
+# DISCOUNT_1000_2000 = 5.5
+# DISCOUNT_2000_3000 = 10
+# DISCOUNT_3000_UP = 30
+#
+#
+# if mani < 1000:
+#     dascaunt =  DISCOUNT_0_1000
+# elif main < 2000:
+#     dascaunt = DISCOUNT_1000_2000
+# elif main < 3000 :
+#     dascaunt = DISCOUNT_2000_3000
+# else:
+#     descaunt = DISCOUNT_3000_UP
+# print(f"Це {descaunt=}")
---
+++ Homework Hillel/24.04.25_2.py
@@ -0,0 +1,6 @@
+lst = [1,2,3,4]
+
+result = lst.insert(0, lst.pop())
+# lst.insert() - вставка елемента по індексу.
+
+print(lst)
